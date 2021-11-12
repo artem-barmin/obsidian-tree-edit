91d74668d7a275da6cfd692b3b6f4cbfce6ba51b
kravich13 <corlack1997@gmail.com>
2021-11-12 17:34:26 +0200

    removed pluginSettings.ts

--- src/PluginSettings.ts
+++
@@ -1,9 +0,0 @@
-import { SplitDirection } from 'obsidian';
-
-export default class TreeEditSettings {
-  splitDirection: SplitDirection = 'horizontal';
-  nodeMinHeight: number = 16;
-  lineHeight: string = '1em';
-  spacingVertical: number = 5;
-  spacingHorizontal: number = 80;
-}
