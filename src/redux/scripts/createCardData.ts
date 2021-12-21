2200f698b44520c835d26a25cc1eb1236f779e0e
kravich13 <corlack1997@gmail.com>
2021-12-21 15:03:43 +0200

    removed unnecessary

--- src/redux/scripts/createCardData.ts
+++ src/redux/scripts/createCardData.ts
@@ -1,4 +1,3 @@
-import { nanoid } from 'nanoid';
 import { ICreateCardData, IPreactState } from '../interfaces';
 
 export const createCardData = ({ id, depth, markdownContent, parents, neighbors, isSelected, isEdit }: ICreateCardData) => {
