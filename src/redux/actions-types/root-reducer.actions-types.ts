fb4ed85df748837aeda6e6382f85a82ed003b536
kravich13 <corlack1997@gmail.com>
2021-12-29 11:56:41 +0200

    fix imports

--- src/redux/actions-types/root-reducer.actions-types.ts
+++ src/redux/actions-types/root-reducer.actions-types.ts
@@ -2,25 +2,22 @@ import { RootInterfaces } from '../interfaces';
 import { IAction } from '../interfaces/IAction';
 import { IDataSelectedElem } from '../interfaces/sharedInterfaces';
 import { RootTypes } from '../types';
-import { ADD_CARD_VERTICALLY, CREATE_EMPTY_CARD } from '../types/root-reducer.types';
 
-const { ADD_CARD, CHANGE_CARD, CHANGE_FIRST_RENDER, CLICK_CARD_VIEW, CREATE_MAIN_STATES, DELETE_CARD } = RootTypes;
+export type CreateMainStates = IAction<typeof RootTypes.CREATE_MAIN_STATES, RootInterfaces.ICreateMainStates>;
 
-export type CreateMainStates = IAction<typeof CREATE_MAIN_STATES, RootInterfaces.ICreateMainStates>;
+export type CreateEmptyCard = IAction<typeof RootTypes.CREATE_EMPTY_CARD, boolean>;
 
-export type CreateEmptyCard = IAction<typeof CREATE_EMPTY_CARD, boolean>;
+export type ChangeFirstRender = IAction<typeof RootTypes.CHANGE_FIRST_RENDER, string>;
 
-export type ChangeFirstRender = IAction<typeof CHANGE_FIRST_RENDER, string>;
+export type ClickCardView = IAction<typeof RootTypes.CLICK_CARD_VIEW, IDataSelectedElem>;
 
-export type ClickCardView = IAction<typeof CLICK_CARD_VIEW, IDataSelectedElem>;
+export type AddCardRight = IAction<typeof RootTypes.ADD_CARD, RootInterfaces.IAddCard>;
 
-export type AddCardRight = IAction<typeof ADD_CARD, RootInterfaces.IAddCard>;
+export type AddCardVertically = IAction<typeof RootTypes.ADD_CARD_VERTICALLY, RootInterfaces.IAddCard>;
 
-export type AddCardVertically = IAction<typeof ADD_CARD_VERTICALLY, RootInterfaces.IAddCard>;
+export type ChangeCard = IAction<typeof RootTypes.CHANGE_CARD, RootInterfaces.IChangeCard>;
 
-export type ChangeCard = IAction<typeof CHANGE_CARD, RootInterfaces.IChangeCard>;
-
-export type DeleteCard = IAction<typeof DELETE_CARD>;
+export type DeleteCard = IAction<typeof RootTypes.DELETE_CARD>;
 
 export type RootAction =
   | CreateMainStates
